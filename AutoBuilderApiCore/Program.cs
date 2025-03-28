ccd235c06ddbffae6cea1013358a7606b5967ea0
Elite <Elite@DESKTOP-RNCLO6C>
2025-03-28 23:11:24 +0300

    generate

--- AutoBuilderApiCore/Program.cs
+++ AutoBuilderApiCore/Program.cs
@@ -9,7 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ApiFolderGenerator>();
+/// <summary>
+/// generate
+///
 if (args.Contains("generate"))
 {
 

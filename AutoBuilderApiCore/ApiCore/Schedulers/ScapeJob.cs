4b10d26c6c337c2d75fca30e5dcdca2cb835dd9b
Elite <Elite@DESKTOP-RNCLO6C>
2025-04-10 16:17:14 +0300

    ScapeJob v1

---
+++ AutoBuilderApiCore/ApiCore/Schedulers/ScapeJob.cs
@@ -0,0 +1,29 @@
+using AutoGenerator.Schedulers;
+
+namespace ApiCore.Schedulers
+{
+
+
+    public class ScapeJob : BaseJob
+    {
+        public override Task Execute(JobEventArgs context)
+        {
+
+
+            Console.WriteLine($"Executing job: {_options.JobName} with cron: {_options.Cron}");
+
+            return Task.CompletedTask;
+        }
+
+        protected override void InitializeJobOptions()
+        {
+
+            // _options.
+            _options.JobName = "space1";
+            _options.Cron=""
+
+        }
+    }
+
+
+}
